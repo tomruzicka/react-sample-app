43b1a5c0bc5c370e0e87f4d58800853f4cd23cf3
Tomáš Růžička <mail.ruzicka@gmail.com>
2024-06-30 23:39:52 +0200

    feat: use react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,33 @@
-import React from 'react';
-import connection from './eWayAPI/Connector';
-import { TContactsResopnse } from './eWayAPI/ContactsResponse';
-import { mergeStyleSets, Dialog, DialogType, DialogFooter, PrimaryButton, ProgressIndicator } from '@fluentui/react';
+import { mergeStyleSets } from "@fluentui/react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout";
+import { Home } from "./pages/Home";
+import { SearchContact } from "./pages/SearchContact";
 
 const css = mergeStyleSets({
-    loadingDiv: {
-        width: '50vw',
-        position: 'absolute',
-        left: '25vw',
-        top: '40vh'
-    }
+  layout: {
+    display: "flex",
+    alignItems: "flex-start",
+  },
 });
 
-const dialogContentProps = {
-    type: DialogType.normal,
-    title: 'Agent Data',
-    isDraggable: false
-};
-
-const modalProps = {
-    isBlocking: true
-};
-
-// This is a React Hook component.
 const App = () => {
-    const [fullName, setFullName] = React.useState<string | null>(null);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/search", element: <SearchContact /> },
+      ],
+    },
+  ]);
 
-    React.useEffect(() => {
-        setTimeout(() => {
-            connection.callMethod(
-                'SearchContacts',
-                {
-                    transmitObject: {
-                        Email1Address: 'mroyster@royster.com' // ealbares@gmail.com, oliver@hotmail.com, michael.ostrosky@ostrosky.com, kati.rulapaugh@hotmail.com and many others
-                    },
-                    includeProfilePictures: false
-                },
-                (result: TContactsResopnse) => {
-                    if (result.Data.length !== 0 && !!result.Data[0].FileAs) {
-                        setFullName(result.Data[0].FileAs);
-                    } else {
-                        setFullName('...top secret...');
-                    }
-                }
-            );
-        },
-            5000
-        );
-    });
-
-    return (
-        <div>
-            <Dialog
-                hidden={!fullName}
-                onDismiss={() => setFullName(null)}
-                dialogContentProps={{ ...dialogContentProps, subText: `His/her name is ${fullName}.` }}
-                modalProps={modalProps}
-            >
-                <DialogFooter>
-                    <PrimaryButton onClick={() => window.location.href = 'https://www.eway-crm.com'} text="OK" />
-                </DialogFooter>
-            </Dialog>
-            {(!fullName) &&
-                <div className={css.loadingDiv}>
-                    <ProgressIndicator label="Loading Agent Name" description="This tape will be destroyed after watching." />
-                </div>
-            }
-        </div>
-    );
+  return (
+    <div className={css.layout}>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
