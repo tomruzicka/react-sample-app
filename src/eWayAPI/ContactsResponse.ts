e75309b7bc6b692e723e9ab550bee1e2d2c101d7
Tomáš Růžička <mail.ruzicka@gmail.com>
2024-06-30 23:40:45 +0200

    feat: add necessary types

--- src/eWayAPI/ContactsResponse.ts
+++ src/eWayAPI/ContactsResponse.ts
@@ -1,9 +1,21 @@
 import { IApiResult } from "@eway-crm/connector";
 
 export type TContactsResopnse = IApiResult & {
-    Data: TContact[]
+  Data: TContact[];
 };
 
 export type TContact = {
-    FileAs: string | null;
+  ItemGUID: string;
+  FileAs: string | null;
+  FirstName: string;
+  LastName: string;
+  Email1Address: string;
+  TelephoneNumber1: string;
+  Title: string;
+  ProfilePicture: string;
+  ItemCreated: string;
+  LastActivity: string;
+  BusinessAddressCity: string;
+  BusinessAddressState: string;
+  BusinessAddressStreet: string;
 };
